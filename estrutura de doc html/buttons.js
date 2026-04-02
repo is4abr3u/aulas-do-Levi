fe53f530946c7be73723388aa14ae494e9f3724d
isadora <isadorasilva.lessa@gmail.com>
2026-04-01 22:17:52 -0300

    FIMSE

---
+++ estrutura de doc html/buttons.js
@@ -0,0 +1,6 @@
+function nome() {
+    alert("Digite seu nome:")   
+}
+
+const buttonexemplo1 = document.getElementById("exercicio1")
+buttonexemplo1.addEventListener('click', () => { exemplo1() })
